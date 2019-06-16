d97d7fc340061359d30a085916740563591378d9
kaisershad <kaisershad@gmail.com>
2019-06-16 18:07:42 +0200

    feat: basic structure

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,19 +1,20 @@
 import * as React from "react";
+import { Header } from "./header";
+import { Body } from "./body";
+import { Footer } from "./footer";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface NotepadProps { compiler: string; framework: string; }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
-export class Hello extends React.Component<HelloProps, {}> {
+export class Notepad extends React.Component<NotepadProps, {}> {
   render() {
     return (
-      <Container maxWidth="sm">
-        <Typography variant="h2" component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
-          Hallo from {this.props.compiler} and {this.props.framework}!
-            </Typography>;
-        </Container>
+      <Container maxWidth="md">
+        <Header />
+        <Body />
+        <Footer />
+      </Container>
     )
   }
 }
---
+++ src/components/body.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export const Body = () => {
+  return(
+    <div>Body</div>
+  )
+}
